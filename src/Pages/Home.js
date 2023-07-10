2b8b48dc6890fddd0428ab6908bf5f8f02a14ed3
Teddy-FN <tedd.ferdy@gmail.com>
2023-07-10 14:39:28 +0700

    Setup UI Home & Navbar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Box } from "@mui/material";
+
+import HeroBanner from "../Component/HeroBanner";
+import SearchExercise from "../Component/SearchExercise";
+import Exercise from "../Component/Exercise";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercise />
+      <Exercise />
+    </Box>
+  );
 };
 
 export default Home;
